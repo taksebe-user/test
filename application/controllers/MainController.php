e16669decee0985aac58e8276a4b622476f55c3f
taksebe <vanousov94@gmail.com>
2022-08-30 22:27:24 +0300

    Changes for creating JSON and XML files

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -4,7 +4,7 @@ namespace application\controllers;
 
 use application\core\Controller;
 use application\core\View;
-
+use \SimpleXMLElement;
 
 class MainController extends Controller
 {
@@ -17,7 +17,19 @@ class MainController extends Controller
         } else {
             //todo curl get data
             //file_put_contents ("filterAll.json",
-            $this->filterDocuments($this->model->getAllData());
+            $files = $this->filterDocuments($this->model->getAllData());
+            file_put_contents(
+                "clearJSON.json",
+                json_encode($files, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE)
+            );
+            $xml_data = new SimpleXMLElement('<?xml version="1.0"?><uslugi></uslugi>');
+
+            // function call to convert array to xml
+            $this->array_to_xml($files, $xml_data);
+
+            //saving generated xml file; 
+            $result = $xml_data->asXML('clearXML.xml');
+            debug($result);
         }
     }
 
@@ -30,10 +42,36 @@ class MainController extends Controller
                 $tmpDocs[] = intval($id);
             }
         }
-        debug($tmpDocs);
+        //debug($tmpDocs);
 
         foreach ($tmpDocs as $id) {
             $this->model->getCurrent($id);
+        }
+
+        if (count($tmpDocs) == 0) {
+            $tmpFiles = array();
+            foreach (array_diff(scandir("files/"), array('.', '..')) as $file) {
+                $tmpFiles[] = json_decode(file_get_contents("files/$file"));
+            }
+
+            return ($tmpFiles);
+        }
+    }
+
+    private function array_to_xml($data, &$xml_data)
+    {
+        foreach ($data as $key => $value) {
+
+            if (is_array($value) or is_object($value)) {
+                if (is_numeric($key)) {
+                    $key = 'usluga' . $key; //dealing with <0/>..<n/> issues
+                }
+                $subnode = $xml_data->addChild($key);
+                $this->array_to_xml($value, $subnode);
+            } else {
+                //debug(["$key", gettype($value)]);
+                $xml_data->addChild("$key", htmlspecialchars("$value"));
+            }
         }
     }
 }
